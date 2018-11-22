5090ea2066267ae1bb319efdca31266188ae1d23
Chris Ha <chunghha@users.noreply.github.com>
2018-11-21 23:49:23 -0600

    feat(client): example of GraphQL Angular client

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+
+import { MatPaginator, MatTableDataSource } from '@angular/material';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { untilDestroyed } from 'ngx-take-until-destroy';
+
+interface Country {
+  name: string;
+  capital?: string;
+  flag?: string;
+  subregion?: string;
+  timezones?: string[];
+}
+
+interface Response {
+  countries: Country[]
+}
 
 @Component({
   selector: 'docker-fs-gql-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements AfterViewChecked, OnDestroy, OnInit {
+
+  response: Response;
+  loading = true;
+
+  displayedColumns = ['name', 'capital', 'subregion'];
+  dataSource: MatTableDataSource<Country>;
+  length = 0;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(private apollo: Apollo) { }
+
+  ngOnInit() {
+    this.apollo
+      .watchQuery({
+        query: gql`
+          {
+            countries {
+              name
+              capital
+              flag
+              subregion
+              timezones
+            }
+          }
+        `,
+      })
+      .valueChanges.pipe(untilDestroyed(this)).subscribe(res => {
+        this.response = <Response>res.data;
+        this.dataSource = new MatTableDataSource<Country>(this.response.countries);
+        this.length = this.response.countries.length;
+        this.loading = false;
+      });
+  }
+
+  ngAfterViewChecked() {
+    if (this.dataSource && this.dataSource.paginator === undefined) {
+      this.dataSource.paginator = this.paginator;
+    }
+  }
+
+  ngOnDestroy() {}
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
--- apps/client/src/app/app.module.ts
+++ apps/client/src/app/app.module.ts
@@ -1,18 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule } from '@angular/router';
+
+import { MatInputModule, MatPaginatorModule, MatTableModule } from '@angular/material';
+import { ApolloModule, APOLLO_OPTIONS } from "apollo-angular";
+import { HttpLinkModule, HttpLink } from "apollo-angular-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { NxModule } from '@nrwl/nx';
 
 import { AppComponent } from './app.component';
-import { NxModule } from '@nrwl/nx';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ApolloModule,
+    HttpLinkModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatTableModule,
     NxModule.forRoot(),
     RouterModule.forRoot([], { initialNavigation: 'enabled' })
   ],
-  providers: [],
+  providers: [{
+    provide: APOLLO_OPTIONS,
+    useFactory(httpLink: HttpLink) {
+      return {
+        cache: new InMemoryCache(),
+        link: httpLink.create({
+          uri: "http://localhost:3333/graphql"
+        })
+      }
+    },
+    deps: [HttpLink]
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
