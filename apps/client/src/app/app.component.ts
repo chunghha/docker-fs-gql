ea8a195d2cae0e13f945b068753575b46a479c74
Chris Ha <chunghha@users.noreply.github.com>
2018-11-21 20:20:37 -0600

    add server/client apps

---
+++ apps/client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'docker-fs-gql-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'client';
+}
