08596e34c8c614d57129dc100012174949515f6f
Chris Ha <chunghha@users.noreply.github.com>
2018-11-23 10:28:59 -0600

    chore: rebuild front by overriding the font family

--- run.sh
+++ run.sh
@@ -5,5 +5,13 @@ if [ "$1" = "rebuild" ]; then
     docker build --tag=docker-fs-gql-front -f ./nginx/Dockerfile .
 fi
 
+if [ "$1" = "api" ]; then
+    docker build --tag=docker-fs-gql-api -f ./api/Dockerfile .
+fi
+
+if [ "$1" = "front" ]; then
+    docker build --tag=docker-fs-gql-front -f ./nginx/Dockerfile .
+fi
+
 docker-compose up -d nginx
 docker-compose up -d api
